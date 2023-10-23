c89dba5675e3facace015cadd76ff746d9a2085a
Matheus Andrade <matheuslincon50@gmail.com>
2023-10-23 16:22:38 +1100

    Initial Commit

---
+++ swift-todo-list/swift_todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_todo_listApp.swift
+//  swift-todo-list
+//
+//  Created by Matheus Andrade on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
