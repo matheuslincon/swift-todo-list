8c35757c45361c6a1a5cf233be694a2a8e254f6a
Matheus Andrade <matheuslincon50@gmail.com>
2023-10-23 16:53:39 +1100

    initial swiftui app with firebase config

--- swift-todo-list/Other/swift_todo_listApp.swift
+++ swift-todo-list/Other/swift_todo_listApp.swift
@@ -5,10 +5,16 @@
 //  Created by Matheus Andrade on 23/10/23.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct swift_todo_listApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
