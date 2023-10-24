29794250ee47d708fc1510ed7ae15c43b5bf4cb9
Matheus Andrade <matheuslincon50@gmail.com>
2023-10-24 14:48:24 +1100

    implemented login view

--- swift-todo-list/Views/LoginView.swift
+++ swift-todo-list/Views/LoginView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // Header
+                HeaderView()
+                // Login form
+                Form {
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    Button {
+                        
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.blue)
+                            
+                            Text("Log In")
+                                .foregroundColor(Color.white)
+                                .bold()
+                        }
+                    }
+                    .padding()
+                }
+                // create account
+                
+                VStack {
+                    Text("New Around here?")
+                    NavigationLink("Create an Account", destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+            }
+        }
     }
 }
 
